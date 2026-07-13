676b4abb3dce0a7635ad397de1fa92a6e8a3a094
Alexander Pak <53107258+alexanderp20@users.noreply.github.com>
2026-07-13 11:29:32 -0700

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.valDict = {}
+        self.minList = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.valDict[val] = self.valDict.get(val, 0) + 1
+        if len(self.minList) > 0:
+            self.minList.append(min(self.minList[-1], val))
+        else: 
+            self.minList.append(val)
+
+    def pop(self) -> None:
+        self.valDict[self.stack[-1]] -= 1
+        self.stack.pop()
+        while len(self.minList) > 0 and self.valDict[self.minList[-1]] == 0:
+            self.minList.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return int(self.minList[-1])
